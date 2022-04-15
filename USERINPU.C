a322e9337d47c150978db62571fde7749649386d
SurajBhil <sab20042000@gmail.com>
2022-04-15 11:14:14 +0530

    ADDED:[Suraj] C Program for User input

---
+++ USERINPU.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	printf("\n Enter the Number : ");
+	scanf("%d%",&a);
+	printf("\n Enter the Number : ");
+	scanf("%d",&b);
+	printf("\n Value of 1st Nmuber :%d",a);
+	printf("\n VAlue of 2nd Number :%d",b);
+	getch();
+}
